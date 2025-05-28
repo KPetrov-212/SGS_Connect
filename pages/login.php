caf0371de62467d9134950517777c5ff24cf2d82
KPetrov212 <krisi.petroff@gmail.com>
2025-05-28 12:19:27 +0300

    Add MySQL dump for users table and remove sign-in component; update login page with form toggle functionality and Bootstrap script

--- pages/login.php
+++ pages/login.php
@@ -158,6 +158,7 @@ if (isset($_POST['submit'])) {
     <footer>
       <?php include '../components/footer.php'; ?>
     </footer>
+
     <script>
       function toggleForms() {
         const signInForm = document.getElementById('signInForm');
@@ -166,6 +167,7 @@ if (isset($_POST['submit'])) {
         signUpForm.classList.toggle('active');
       }
     </script>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
   </body>
 </html>
