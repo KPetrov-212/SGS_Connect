0e79ec7ac09310ab617721573c1e145d7e088cb9
Kristian Petrov <krisi.petroff@gmail.com>
2025-06-24 00:15:21 +0300

    Add AUTO_INCREMENT to `id` column in `users` table definition

--- db/init.sql
+++ db/init.sql
@@ -90,7 +90,7 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `password` varchar(255) NOT NULL,
