5f0c6ddf01390d40a06295ac8dc142e5a52a99db
KPetrov212 <krisi.petroff@gmail.com>
2025-06-24 14:20:44 +0300

    db fix

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,27 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: sgs
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `orders`
+--
 
 DROP TABLE IF EXISTS `orders`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
@@ -16,15 +36,24 @@ CREATE TABLE `orders` (
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`panel_id`) REFERENCES `panels` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
+--
+-- Dumping data for table `orders`
+--
 
 LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
+--
+-- Table structure for table `panels`
+--
 
 DROP TABLE IF EXISTS `panels`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `panels` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `model_name` varchar(100) DEFAULT NULL,
@@ -41,16 +70,25 @@ CREATE TABLE `panels` (
   `image_url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `panels`
+--
 
 LOCK TABLES `panels` WRITE;
+/*!40000 ALTER TABLE `panels` DISABLE KEYS */;
 INSERT INTO `panels` VALUES (1,'SunPro X200','SolarTech Industries',200,17.5,'1480x670x35',12.5,'Polycrystalline',22.4,8.9,95.99,10,'images/x200.png'),(2,'Energex Ultra300','Energex Ltd.',300,20.1,'1640x992x40',18,'Monocrystalline',34.2,8.77,129.99,25,'images/ultra300.png'),(3,'EcoLite Thin120','GreenCore Solutions',120,12.8,'1200x540x5',6.8,'Thin-Film',18.5,6.5,59.5,5,'images/thin120.png'),(4,'Photon Max360','Photonics Corp.',360,21.8,'1700x1000x35',19.3,'Monocrystalline',38,9.5,179,30,'images/max360.png'),(5,'Solaro Edge250','Solaro Inc.',250,18.6,'1650x990x40',15.4,'Polycrystalline',29.5,8.47,109.75,15,'images/edge250.png');
+/*!40000 ALTER TABLE `panels` ENABLE KEYS */;
 UNLOCK TABLES;
 
-
+--
+-- Table structure for table `users`
+--
 
 DROP TABLE IF EXISTS `users`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
@@ -59,12 +97,25 @@ CREATE TABLE `users` (
   `role` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `users`
+--
 
 LOCK TABLES `users` WRITE;
-
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (3,'KK','k@gmail.com','$2y$10$KJlQT.t2u7sRzSTJSGZ52.qUFSug.krWrbgrQcjLJg/vuQkOQP0mG',NULL),(4,'K-K','goto@gmail.com','$2y$10$OOFww/KqtYwjPNYPjhKWP.s0m85r/MkmDS8sf5p2WmymeB7tyLsju',NULL);
-
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2025-06-18 10:11:24
